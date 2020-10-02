db528ee0c5955c716f216568ffac073b30acc442
Kenji <kenji.au9@gmail.com>
2020-10-02 15:06:10 -0600

    Added in input display & changed input to POST;

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   // Start the session.
   session_start();
 
-  if (!empty($_GET))
+  if (!empty($_POST))
   { // Make sure that the request is not empty.
     if (!$_SESSION["to-do-list"])
     { // If the to-do-list doesn't initialize.
@@ -13,7 +13,7 @@
       $toDoList = $_SESSION["to-do-list"];
     }
     // Add to the to-do-list.
-    array_push($toDoList, $_GET["new-to-do"] );
+    array_push($toDoList, $_POST["new-to-do"] );
     
     // Update the session variable.
     $_SESSION["to-do-list"] = $toDoList;
@@ -29,13 +29,21 @@
 <body>
   <h1>PHP To Do List</h1>
   <h2>Add a To-Do</h1>
-  <form method="GET" action="index.php">
+  <form method="POST" action="index.php">
     <label for="new-to-do-item">
       <input type="text" id="new-to-do-item" name="new-to-do" required=true;>
     </label>
     <input type="submit" value="Add Item" />
   </form>
+
   <h2>Active To-Dos</h2>
+  <?php if (isset($_SESSION['to-do-list'])) : ?>
+    <?php foreach( $_SESSION['to-do-list'] as $toDoItem) : ?>
+      <input type="checkbox" />
+      <label><?php echo $toDoItem; ?></label>
+    <?php endforeach ?>
+  <?php endif ?>
+  
   <h2>Completed To-Dos</h2>
   <h2>Debugging</h2>
   <?php var_dump($_SESSION); ?>
